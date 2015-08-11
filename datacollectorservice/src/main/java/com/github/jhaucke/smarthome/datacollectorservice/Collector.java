5f2c21060aa4f119d23c0735b7a4f9f6f8dcb0c1
Jeremias <jeremias.haucke@NB350.saxsys.de>
2015-08-11 15:03:40 +0200

    init datacollectorservice

---
+++ datacollectorservice/src/main/java/com/github/jhaucke/smarthome/datacollectorservice/Collector.java
@@ -0,0 +1,13 @@
+package com.github.jhaucke.smarthome.datacollectorservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class Collector 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
