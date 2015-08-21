30d774fce10e2b33cb9139e76b73a6da13392abc
Jeremias <jeremias.haucke@NB350.saxsys.de>
2015-08-21 13:58:12 +0200

    new project watchdogservice

---
+++ watchdogservice/src/main/java/com/github/jhaucke/smarthome/watchdogservice/WatchDog.java
@@ -0,0 +1,13 @@
+package com.github.jhaucke.smarthome.watchdogservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class WatchDog 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
